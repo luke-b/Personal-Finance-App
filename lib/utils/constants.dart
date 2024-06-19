6ccc4a8545c02e5d7a7d367a1c4a8f50e86455dd
luke-b <luke.benda@gmail.com>
2024-06-19 11:00:31 +0200

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String appName = 'Personal Finance App';
+  static const String databaseName = 'personal_finance_app.db';
+  static const int databaseVersion = 1;
+  static const String userTable = 'users';
+  static const String transactionTable = 'transactions';
+  static const String budgetTable = 'budgets';
+  static const String goalTable = 'financial_goals';
+}
